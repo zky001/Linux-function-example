4421a017a793248dfcb8ab369ab689a739e75c05
zky001 <1134386961@qq.com>
2015-05-23 21:38:43 +0800

    POLL
    poll is a way to detection

--- POLL.cpp
+++ POLL.cpp
@@ -1,22 +1,26 @@
 #include <stdio.h>
-#include <sys/select.h>
+#include "stdlib.h"
+#include "poll.h"
 #include "fcntl.h"
 #include "unistd.h"
 #define MAXNUM 100
 
 int main() {
-    fd_set rfds;
-    struct timeval tv;
+    struct pollfd fds[2];
     int retval,fd;
     char buf[MAXNUM];
     fd=open("/dev/input/mice",O_RDONLY);
+    if (fd<0){
+        perror("open");
+        exit(-1);
+    }
+    fds[0].fd=0;
+    fds[0].events=POLLIN;
+    fds[1].fd=fd;
+    fds[1].events=POLLIN;
     while (1){
-        FD_ZERO(&rfds);
-        FD_SET(0,&rfds);
-        FD_SET(fd,&rfds);
-        tv.tv_sec=5;
-        tv.tv_usec=0;
-        retval=select(fd+1,&rfds,NULL,NULL,&tv);
+        retval=poll(fds,2,5000);
+        printf("retval=%d\n",retval);
         if (retval<0){
             printf("error\n");
         }
@@ -24,11 +28,11 @@ int main() {
             printf("NO DATA within 5 seconds\n");
         }
         if (retval>0){
-            if (FD_ISSET(0,&rfds)){
+            if (fds[0].revents&POLLIN){
                 printf("data from keyboard now\n");
                 read(0,buf,MAXNUM);
             }
-            if (FD_ISSET(fd,&rfds)){
+            if (fds[1].revents&POLLIN){
                 printf("data is available from mouse now\n");
                 read(fd,buf,MAXNUM);
             }
